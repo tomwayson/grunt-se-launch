7c2ed696901811974a81d5c4e8b3732b633fec35
Brian Bunker <bbunker@esri.com>
2014-05-27 14:33:33 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-grunt-se-launch
-===============
+# grunt-selenium-launcher
+
+A Grunt task to launch a selenium server.
+
+## Usage
+Set up configuration options:
+
+```javascript
+grunt.initConfig({
+	'selenium-launch': {
+    options: {
+      port: 4445,
+      jarDir: 'C:/Selenium/',
+      jar: 'selenium-server-standalone-2.41.0.jar'
+    }
+  }
+})
+```
+
+Load the task:
+
+```javascript
+grunt.loadNpmTasks('grunt-se-launch');
+```
+
+Call the task:
+
+```javascript
+grunt.registerTask('test', [ 'selenium-launch', 'intern'])
+```
+
+Use the settings exported to the environment to connect to the correct instance:
+
+```javascript
+var driver = new require("selenium-webdriver").Builder()
+	.usingServer(process.env.SELENIUM_HUB)
+	.withCapabilities(webdriver.Capabilities.firefox())
+	.build()
+```
+
+## API
+
+### Grunt Task
+
+**selenium-launch**
+Attempt to launch a selenium instance, binding to `process.env.SELENIUM_LAUNCHER_PORT`. The task completes when the server is running and bound. The server is automatically killed when the grunt process exits - no need to take extreme steps to ensure the process isn't left a zombie.
+
+### Environment Variables
+
+**process.env.SELENIUM_LAUNCHER_PORT**
+If set when running grunt, defines the preferred port to run Selenium on. After the task has run, has the value of the port selenium bound on.
+
+**process.env.SELENIUM_HUB**
+After the task has run, has the string URI for webdriver hub connection. Use this string when connecting clients to selenium.
---
+++ tasks/grunt-selenium-launcher.js
@@ -0,0 +1,73 @@
+var spawn = require('child_process').spawn,
+	EventEmitter = require('events').EventEmitter,
+	util = require('util');
+
+module.exports = function(grunt) {
+	var selenium = null;
+
+	var End = function() {
+		if (selenium && selenium.kill) {
+			selenium.kill();
+		}
+	};
+
+	// NO FULLY AUTOMATED TESTS FOR THIS
+	// There is a semi-auto test via `grunt testExit`, but requires user confirmation.
+	process.on('exit', End);
+
+	grunt.registerTask('selenium-launch', 'Start a selenium remote.', function() {
+		var done = this.async();
+		var options = this.options();
+
+		var cb = function(err, sel) {
+			if (err) {
+				// semi-auto test via `grunt testInduceFailure`
+				grunt.log.error('There was a problem launching selenium.');
+				grunt.log.debug(err);
+				done(false);
+			} else {
+				selenium = sel;
+				process.env.SELENIUM_LAUNCHER_PORT = selenium.port
+				process.env.SELENIUM_HUB = "http://localhost:" + process.env.SELENIUM_LAUNCHER_PORT + "/wd/hub"
+				done();
+			}
+		};
+
+		function run(cb) {
+			console.log('Starting Selenium on port ' + options.port);
+			var child = spawn('java', [
+				'-jar', options.jarDir + options.jar,
+				'-port', options.port
+			]);
+			child.host = '127.0.0.1';
+			child.port = options.port;
+			var badExit = function() {
+				cb(new Error('Could not start Selenium.'));
+			};
+			child.stdout.on('data', function(data) {
+				var sentinal = 'Started org.openqa.jetty.jetty.Server';
+				if (data.toString().indexOf(sentinal) != -1) {
+					child.removeListener('exit', badExit);
+					cb(null, child);
+				}
+			});
+			child.on('exit', badExit);
+		}
+
+		function FakeProcess(port) {
+			EventEmitter.call(this);
+			this.host = '127.0.0.1';
+			this.port = port;
+		}
+		util.inherits(FakeProcess, EventEmitter);
+		FakeProcess.prototype.kill = function() {
+			this.emit('exit');
+		};
+
+		if (process.env.SELENIUM_LAUNCHER_PORT) {
+			return process.nextTick(
+				cb.bind(null, null, new FakeProcess(process.env.SELENIUM_LAUNCHER_PORT)));
+		}
+		run(cb);
+	});
+};
